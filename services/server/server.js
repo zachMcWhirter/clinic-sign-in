3f8c306193d57ffb25c9eb39ea2ea96a6db137d2
Zach McWhirter <zmac7811@gmail.com>
2021-01-18 13:52:05 -0600

    added to server.js

--- services/server/server.js
+++ services/server/server.js
@@ -1,3 +1,12 @@
-'use strict';
+"use strict";
 
-const app = require('express')();
+const app = require("express")();
+const PORT = process.env.PORT || "10020";
+
+app.get("/hello", function (req, res) {
+  return res.status(200).send({ hello: "world" });
+});
+
+app.listen(PORT, function () {
+  console.log(`app listening on port ${PORT}`);
+});
